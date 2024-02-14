aa9e1021bb0a61433f62967a92637052d1963171
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-02-14 22:53:59 +0300

    feat: implement DataAccess layer

--- Identity.DataAccess/IdentityContext.cs
+++ Identity.DataAccess/IdentityContext.cs
@@ -1,10 +1,17 @@
 ﻿using Identity.Core.Models;
+using Identity.DataAccess;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Identity.Data
 {
-    public class IdentityContext : IdentityDbContext<User>
+    public class IdentityContext(DbContextOptions options) : IdentityDbContext<User>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
 
+            new UserSeed(builder).Seed();
+        }
     }
 }
---
+++ Identity.DataAccess/UserSeed.cs
@@ -0,0 +1,79 @@
+﻿using Identity.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.DataAccess
+{
+    public class UserSeed(ModelBuilder modelBuilder)
+    {
+        private readonly ModelBuilder _modelBuilder = modelBuilder;
+
+        public void Seed()
+        {
+            var adminRoleId = Guid.NewGuid().ToString();
+            var userRoleId = Guid.NewGuid().ToString();
+
+            //Roles
+            _modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = adminRoleId,
+                    ConcurrencyStamp = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole
+                {
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId,
+                    Name = "User",
+                    NormalizedName = "USER"
+                });
+
+            var adminId = Guid.NewGuid().ToString();
+            var adminUser = new User
+            {
+                Id = adminId,
+                Email = "admin@admin.com",
+                EmailConfirmed = true,
+                UserName = "AdminTest",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                NormalizedUserName = "ADMINTEST"
+            };
+
+            var userId = Guid.NewGuid().ToString();
+            var regularUser = new User
+            {
+                Id = userId,
+                Email = "user@example.com",
+                EmailConfirmed = false,
+                UserName = "JoeDoe",
+                NormalizedUserName = "JOEDOE",
+                NormalizedEmail = "USER@EXAMPLE.COM"
+            };
+
+            //Add Hash Password
+            var passwordHasher = new PasswordHasher<User>();
+            adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "0451");
+            regularUser.PasswordHash = passwordHasher.HashPassword(regularUser, "F_p@raD0x?");
+
+
+            _modelBuilder.Entity<User>().HasData(
+                adminUser,
+                regularUser);
+
+            //Link Test Users with Roles
+            _modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRoleId,
+                    UserId = adminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRoleId,
+                    UserId = userId
+                });
+        }
+    }
+}
