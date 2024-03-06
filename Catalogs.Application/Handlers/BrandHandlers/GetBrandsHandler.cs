ada75dbeefab8c43a829ed1d5539d02fe9365ed0
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-03-06 22:29:40 +0300

    refactor: remove test log

--- Catalogs.Application/Handlers/BrandHandlers/GetBrandsHandler.cs
+++ Catalogs.Application/Handlers/BrandHandlers/GetBrandsHandler.cs
@@ -1,19 +1,16 @@
 ﻿using Catalogs.Application.DataTransferObjects;
 using Catalogs.Application.Queries.BrandQueries;
 using MediatR;
-using Microsoft.Extensions.Logging;
-using Serilog;
 
 namespace Catalogs.Application.Handlers.BrandHandlers
 {
-    public sealed class GetBrandsHandler(IUnitOfWork unitOfWork, IMapper mapper, ILogger<GetBrandsHandler> logger) : IRequestHandler<GetBrandsQuery, IEnumerable<BrandDto>>
+    public sealed class GetBrandsHandler(IUnitOfWork unitOfWork, IMapper mapper) : IRequestHandler<GetBrandsQuery, IEnumerable<BrandDto>>
     {
         private readonly IUnitOfWork _unitOfWork = unitOfWork;
         private readonly IMapper _mapper = mapper;
 
         public async Task<IEnumerable<BrandDto>> Handle(GetBrandsQuery query, CancellationToken token)
         {
-            logger.LogInformation("TEST MESSAGE FIOR JIBANA");
             var brand = await _unitOfWork.Brand.GetAllBrandsAsync(query.TrackChanges, token);
 
             var brandDtos = _mapper.Map<IEnumerable<BrandDto>>(brand);
