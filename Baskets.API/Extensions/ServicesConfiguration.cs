cb097d36a60ac34281c2bac6df48264ba1fed35e
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-04-05 22:38:39 +0300

    refactor: remove redutant usings

--- Baskets.API/Extensions/ServicesConfiguration.cs
+++ Baskets.API/Extensions/ServicesConfiguration.cs
@@ -3,8 +3,6 @@ using Baskets.DataAccess.DBContext;
 using Baskets.DataAccess.Entities.Models;
 using Baskets.DataAccess.UnitOfWork;
 using FluentValidation;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
 using System.Text;
